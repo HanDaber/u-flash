de7bfb93ed50cb85e87a6227904084b1f430edbb
handaber <handaber@gmail.com>
2016-04-20 03:25:27 -0700

    save

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,8 @@
 /*! @preserve dh process.env.APP_HASH */
 import './index.html'
-// import './favicon.ico'
-// import 'normalize.css/normalize.css'
-// import './scss/app.scss'
+import './favicon.ico'
+import 'normalize.css/normalize.css'
+import './scss/app.scss'
 
 import FlashClass from './components/Flash/flash'
 if( ! FlashClass.isRegistered('u-flash') ){
